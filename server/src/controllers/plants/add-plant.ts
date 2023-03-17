e1202b566806e45b530a73e1921693240c862af8
Brian Schwalm <bschwalm@anythinksolutions.com>
2023-03-17 13:21:56 -0400

    added begining of addPlant route.

---
+++ server/src/controllers/plants/add-plant.ts
@@ -0,0 +1,9 @@
+import { RequestHandler } from "express";
+// import fbService from "../../lib/firebase";
+
+const addPlant: RequestHandler = async (req, res) => {
+
+  res.status(201).json({message: "not implemented"});
+}
+
+export default addPlant;
--- server/src/routes/plants.ts
+++ server/src/routes/plants.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import listPlants from "../controllers/plants/list-plants";
 import scrapePlants from "../controllers/plants/scrape-plants";
+import addPlant from "../controllers/plants/add-plant";
 
 const plantRoutes = express.Router()
 
 plantRoutes.get("/", listPlants);
 plantRoutes.get("/scrape", scrapePlants);
+plantRoutes.post("/", addPlant);
 
 export default plantRoutes;
