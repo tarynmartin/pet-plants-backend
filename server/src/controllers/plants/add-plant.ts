717b420125bdc5c5ed6669e00fb9f102a5507b89
Taryn Martin <taryn@fairwinds.com>
2023-03-17 11:42:46 -0600

    added logic for adding plants

--- server/src/controllers/plants/add-plant.ts
+++ server/src/controllers/plants/add-plant.ts
@@ -1,9 +1,10 @@
 import { RequestHandler } from "express";
-// import fbService from "../../lib/firebase";
+import fbService from "../../lib/firebase";
 
 const addPlant: RequestHandler = async (req, res) => {
-
-  res.status(201).json({message: "not implemented"});
+  console.log('request', req.body);
+  const docRef = await fbService().db.collection('plants').add(req.body);
+  res.status(201).json({message: `item added: ${docRef.id}`});
 }
 
 export default addPlant;
