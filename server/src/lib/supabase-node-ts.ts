64f515dc81e8d4b9e909aa0c2da9148b790c3fe3
Taryn Martin <taryn@fairwinds.com>
2023-05-19 16:29:50 -0600

    removed require statement

--- server/src/lib/supabase-node-ts.ts
+++ server/src/lib/supabase-node-ts.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
+import serviceAccount from './supabase-service-account.json';
 
 let service: SupabaseClient<any, "public", any> | null = null;
 
@@ -9,10 +10,8 @@ const supabaseService = () => {
     console.log("initializing the supabase service.");
     
     //TODO: create this file, based on supabase-example.json, and then paste in real values from supabase
-    const serviceAccountFilePath = "./supabase-service-account.json";
-    const creds = require(serviceAccountFilePath);
 
-    const supabase = createClient(creds.projectUrl, creds.serviceKey);
+    const supabase = createClient(serviceAccount.projectUrl, serviceAccount.serviceKey);
     service = supabase;
   }
 
