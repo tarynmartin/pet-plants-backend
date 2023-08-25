8e72a2bac0696e8e9276c70184646c77fd73e2be
Taryn Martin <taryn@fairwinds.com>
2023-08-25 15:50:10 -0600

    fix: added endpoint to search and filter at the same time

--- server/src/controllers/plants/toxic-plants-by-animal.ts
+++ server/src/controllers/plants/toxic-plants-by-animal.ts
@@ -3,16 +3,10 @@ import sbDatabase from "../../lib/supabase-node-ts";
 import { handleResponse } from '../helpers';
 
 const toxicPlantsByAnimal: RequestHandler = async (req, res) => {
-  if (req.body.search) {
-    const { data, error } = await sbDatabase().from("plants").select().textSearch('search_plants', req.body.search).eq(req.params.animal === 'cats' ? 'toxic_cats' : 'toxic_dogs', req.params.toxicity)
+  const { data, error } = await 
+  sbDatabase().from("plants").select().is(req.params.animal === 'cats' ? 'toxic_cats' : 'toxic_dogs', req.params.toxicity)
 
-    handleResponse(res, data, error)
-  } else {
-    const { data, error } = await 
-    sbDatabase().from("plants").select().is(req.params.animal === 'cats' ? 'toxic_cats' : 'toxic_dogs', req.params.toxicity)
-
-    handleResponse(res, data, error)
-  }
+  handleResponse(res, data, error)
 }
 
 export default toxicPlantsByAnimal;
